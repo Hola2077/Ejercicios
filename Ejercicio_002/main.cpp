4291ee91c241d673f19a6cab160c061f1d5f0f06
Hola2077 <113859878+Hola2077@users.noreply.github.com>
2022-10-01 14:38:22 -0500

    Update main.cpp

--- Ejercicio_002/main.cpp
+++ Ejercicio_002/main.cpp
@@ -6,23 +6,20 @@ class Inprimir
 {
 public:
     void va(){system("clear");} //"cls" en windows
-    int uno()
-    {
+    int uno(){
         int A = 0;
         cout << "1 = Suma" << endl;
         cout << "Que operacion va a realisar: ";
         cin >> A;va();
         return A;
     }
-    int fila()
-    {
+    int fila(){
         int A = 0;
         cout << "Ingrese numero el numero de filas(1 ~ 20): ";
         cin >> A;
         return A;
     }
-    int columna()
-    {
+    int columna(){
         int A = 0;
         cout << "Ingrese numero el numero de columnas(1 ~ 20): ";
         cin >> A;
@@ -34,19 +31,29 @@ public:
 class Matris
 {
 public:
+
     void forma(){
     Inprimir inprimir;
-    int sele = 0, fila = 0, columna = 0;
-    sele = inprimir.uno();
-    if (sele == 1){
-        fila = inprimir.fila();
-        columna = inprimir.columna();
-    }else{cout << "Seleccion no valida";}
+    inSele(inprimir.uno());
+    if (outSele() == 1){
+        inFila(inprimir.fila());
+        inColumna(inprimir.columna());
+        inprimir.va();
+        if(outFila() == 0 && outColumna() == 0){cout << "No puede tener 0 filas o 0 columnas"<< endl;}
+    }else{cout << "Seleccion no valida"<<endl;}
     }
+
+    int outSele(){return sele;}
+    int outFila(){return fila;}
+    int outColumna(){return columna;}
+
+    void inSele(int A){sele = A;}
+    void inFila(int A){fila = A;}
+    void inColumna(int A){columna = A;}
 
 
 private: 
-    int SE,FI,CO;
+    int SE,FI,CO,sele = 0, fila = 0, columna = 0;
 };
 
 int main()
