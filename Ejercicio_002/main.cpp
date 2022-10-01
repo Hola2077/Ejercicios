5f545341864ab455bec3e2880575afe2091d82b0
Hola2077 <113859878+Hola2077@users.noreply.github.com>
2022-10-01 13:22:42 -0500

    Update main.cpp

--- Ejercicio_002/main.cpp
+++ Ejercicio_002/main.cpp
@@ -1,58 +1,58 @@
-///Operaciones con matrices basicas////
+/// Operaciones con matrices basicas////
 #include <iostream>
-#include <stdlib.h>  
+#include <stdlib.h>
 using namespace std;
-
-class Operaciones{
-public: 
-	void seleccion(int A){
-		switch (A){
-			case 1: suma();
-			break;
-			default:system("cls"); cout << "Opcion no valida";
-			break;
-		}
-	}
-	void suma(){
-		
-	}
-	int Ssuma(int A){return A;}
-private:
-	
-};
-class Inprimir{
-	Operaciones op;
+class Inprimir
+{
 public:
-	int iSuma(){
-		cout << "1 = 2x2";
-		cout << "2 = 3x3";
-		cout << "3 = 4x4";
-		cout << "ingrese el formato dela matriz";int A;
-		cin >> A;op.Ssuma(A);
-	}
-	int seleccion(){
-		return O;
-	}
-	void uno(){
-		cout << "1 = Suma" <<"\n";
-		cout << "Ingrese la operacion: ";
-		cin >> O;
-		switch (O){
-			case 1: op.suma();
-			break;
-			default:system("cls"); cout << "Opcion no valida";
-			break;
-		}
-	}
+    void va(){system("clear");} //"cls" en windows
+    int uno()
+    {
+        int A = 0;
+        cout << "1 = Suma" << endl;
+        cout << "Que operacion va a realisar: ";
+        cin >> A;va();
+        return A;
+    }
+    int fila()
+    {
+        int A = 0;
+        cout << "Ingrese numero el numero de filas(1 ~ 20): ";
+        cin >> A;
+        return A;
+    }
+    int columna()
+    {
+        int A = 0;
+        cout << "Ingrese numero el numero de columnas(1 ~ 20): ";
+        cin >> A;
+        return A;
+    }
+};
+    
+
+class Matris
+{
+public:
+    void forma(){
+    Inprimir inprimir;
+    int sele = 0, fila = 0, columna = 0;
+    sele = inprimir.uno();
+    if (sele == 1){
+        fila = inprimir.fila();
+        columna = inprimir.columna();
+    }else{cout << "Seleccion no valida";}
+    }
+
+
 private: 
-	int O;
+    int SE,FI,CO;
 };
 
-int main(){
-	Inprimir inprimir;
-	Operaciones op;
-	inprimir.uno();
-	op.seleccion(inprimir.seleccion());
-	
-	
+int main()
+{
+    Matris matris;
+    matris.forma();
+    
+
 }
