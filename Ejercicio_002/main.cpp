1d3386507e02535dca957238f3d76395661ef786
Hola2077 <113859878+Hola2077@users.noreply.github.com>
2022-10-01 15:29:02 -0500

    Update main.cpp

--- Ejercicio_002/main.cpp
+++ Ejercicio_002/main.cpp
@@ -6,31 +6,49 @@ class Inprimir
 {
 public:
     void va(){system("clear");} //"cls" en windows
+    void inA(int A){AA = A;};int outA(){return AA;}; //fila 
+    void inB(int B){BB = B;};int outB(){return BB;}; //columna
+    void inC(int C){CC = C;};int outC(){return CC;}; //
     int uno(){
         int A = 0;
         cout << "1 = Suma" << endl;
         cout << "Que operacion va a realisar: ";
-        cin >> A;va();
-        return A;
-    }
+        cin >> A;inC(A);va();return A;}
     int fila(){
         int A = 0;
         cout << "Ingrese numero el numero de filas(1 ~ 20): ";
-        cin >> A;
-        return A;
-    }
+        cin >> A;inA(A);return A;}
     int columna(){
         int A = 0;
         cout << "Ingrese numero el numero de columnas(1 ~ 20): ";
-        cin >> A;
-        return A;
+        cin >> A;inB(A);return A;}
+ 
+    void cosMatris(int A,int B){
+        //int matris[20][20];
+        while(A == outA() && B == outB()){
+
+        }
+        cout << "Ingrese el valor de "<<A<<"."<<B; 
+
     }
+private:
+    int AA,BB,CC;
 };
     
 
 class Matris
 {
 public:
+    int outSele(){return sele;}//salida
+    int outFila(){return fila;}
+    int outColumna(){return columna;}
+    
+    void inSele(int A){sele = A;}//entrada
+    void inFila(int A){fila = A;}
+    void inColumna(int A){columna = A;}
+    
+    void inS(bool A){YY = A;}//entrada
+    bool inS(){return YY;}//salida
 
     void forma(){
     Inprimir inprimir;
@@ -43,23 +61,16 @@ public:
     }else{cout << "Seleccion no valida"<<endl;}
     }
 
-    int outSele(){return sele;}
-    int outFila(){return fila;}
-    int outColumna(){return columna;}
 
-    void inSele(int A){sele = A;}
-    void inFila(int A){fila = A;}
-    void inColumna(int A){columna = A;}
 
 
 private: 
-    int SE,FI,CO,sele = 0, fila = 0, columna = 0;
+    int sele = 0, fila = 0, columna = 0;
+    bool YY;
 };
 
 int main()
 {
     Matris matris;
     matris.forma();
-    
-
 }
