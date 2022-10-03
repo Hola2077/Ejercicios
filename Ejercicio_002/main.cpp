4ed1a96012a02c62fdc031801a10af865d1b2b8b
Hola2077 <113859878+Hola2077@users.noreply.github.com>
2022-10-02 20:08:07 -0500

    Update main.cpp

--- Ejercicio_002/main.cpp
+++ Ejercicio_002/main.cpp
@@ -8,7 +8,7 @@ public:
     void va(){system("clear");} //"cls" en windows
     void inA(int A){AA = A;};int outA(){return AA;}; //fila 
     void inB(int B){BB = B;};int outB(){return BB;}; //columna
-    void inC(int C){CC = C;};int outC(){return CC;}; //
+    void inC(int C){CC = C;};int outC(){return CC;}; //Operacion 
     int uno(){
         int A = 0;
         cout << "1 = Suma" << endl;
@@ -23,18 +23,29 @@ public:
         cout << "Ingrese numero el numero de columnas(1 ~ 20): ";
         cin >> A;inB(A);return A;}
  
-    void cosMatris(int A,int B){
-        //int matris[20][20];
-        while(A == outA() && B == outB()){
-
+    void cosMatris(){
+        int F = 0;int C = 0;int D = 0; //filas, columnas y valor a asignar
+        bool orden = false; //terminar bucle
+        int matris[20][20] = {0};va();
+        while(orden == false){
+            va();
+            for (int A=0;A<outA();A ++){
+                for (int F=0;F<outB();F++){
+                    cout<<" "<<matris[A][F];
+                }
+                cout<<endl;
+            }
+            cout << "Ingrese el valor de "<<F+1<<"."<<C+1<<": ";
+            cin>>D;
+            matris[F][C] = D;
+            if(F != outA()){F ++;};if(C != outA()){C ++;};
         }
-        cout << "Ingrese el valor de "<<A<<"."<<B; 
+        
 
     }
 private:
     int AA,BB,CC;
 };
-    
 
 class Matris
 {
@@ -57,6 +68,7 @@ public:
         inFila(inprimir.fila());
         inColumna(inprimir.columna());
         inprimir.va();
+        inprimir.cosMatris();
         if(outFila() == 0 && outColumna() == 0){cout << "No puede tener 0 filas o 0 columnas"<< endl;}
     }else{cout << "Seleccion no valida"<<endl;}
     }
